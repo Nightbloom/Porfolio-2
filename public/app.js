9b66ec9c7a4e5dd23240ecbd0d5a0cd7ae681df3
Siddharth Sharma <siddharthsharma653@gmail.com>
2022-01-03 21:39:39 +0530

    first commit

---
+++ public/app.js
@@ -0,0 +1,10 @@
+// links
+
+const links = document.querySelectorAll('.links');
+
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        links.forEach(ele => ele.classList.remove('active'));
+        link.classList.add('active');
+    })
+})
---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+let initialPath = path.join(__dirname, "public")
+
+app.use(express.static(initialPath));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(initialPath, "index.html"));
+})
+
+app.listen('3000', () => {
+    console.log("Listening at https://localhost:3000");
+})
