e94df7302d79e27f6ca046f033170ed914136139
Siddharth Sharma <siddharthsharma653@gmail.com>
2022-01-04 18:16:46 +0530

    3rd commit

---
+++ public/project.js
@@ -0,0 +1,42 @@
+let projects = [
+    {
+        name: "project one",
+        tags: "#javascript, #fullstack",
+        image: "project1.jpg",
+    },
+    {
+        name: "project one",
+        tags: "#ui/ux, #backend",
+        image: "project1.jpg",
+    },
+    {
+        name: "project one",
+        tags: "#javascript, #fullstack, #backend",
+        image: "project1.jpg",
+    },
+    {
+        name: "project one",
+        tags: "#javascript, #fullstack, #ui/ux, #backend",
+        image: "project1.jpg",
+    },
+    {
+        name: "project one",
+        tags: "#javascript, #fullstack, #ui/ux",
+        image: "project1.jpg",
+    },
+    {
+        name: "project one",
+        tags: "#javascript, #fullstack, #backend",
+        image: "project1.jpg",
+    },
+    {
+        name: "project one",
+        tags:  "#fullstack, #ui/ux, #backend",
+        image: "project1.jpg",
+    },
+    {
+        name: "project one",
+        tags: "#javascript, #ui/ux, #backend",
+        image: "project1.jpg",
+    },
+]
