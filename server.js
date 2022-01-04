b737876183f7b95f29c85d404e72cc6c627362a7
Siddharth Sharma <siddharthsharma653@gmail.com>
2022-01-04 22:37:01 +0530

    5th commit

--- server.js
+++ server.js
@@ -15,7 +15,30 @@ app.get('/', (req, res) => {
 })
 
 app.post('/mail', (req, res) => {
-    
+    const { firstname, lastname, email, msg} = req.body;
+    console.log(req.body);
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.PASSWORD
+        }
+    })
+    const mailOptions = {
+        from: 'nbloom091@gmail.com',
+        to: 'nbloom091@gmail.com',
+        subject: 'Portfolio',
+        text: `First name: ${firstname}, \nLast name: ${lastname}, \nEmail: ${email}, \nMessage: ${msg}`
+    }
+
+    transporter.sendMail(mailOptions, (err, result) => {
+        if(err){
+            console.log(err);
+            res.json('oops! it seem like some error occured plz');
+        }else{
+            res.json('thanks for e-mailing me. I will reply to you within 2 working days');
+        }
+    })
 })
 
 app.listen('3000', () => {
