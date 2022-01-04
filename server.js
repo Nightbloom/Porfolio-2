b5cd2f1e974795a8292175ab3b8c6ea98256add2
Siddharth Sharma <siddharthsharma653@gmail.com>
2022-01-04 18:47:29 +0530

    4th commit

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const nodemailer = require('nodemailer');
+const dotenv = require('dotenv');
 
+dotenv.config();
 let initialPath = path.join(__dirname, "public")
 
 app.use(express.static(initialPath));
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(initialPath, "index.html"));
+})
+
+app.post('/mail', (req, res) => {
+    
 })
 
 app.listen('3000', () => {
