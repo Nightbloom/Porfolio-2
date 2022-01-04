65a7a35059a3c088ff16059bd23461ef31317fa1
Siddharth Sharma <siddharthsharma653@gmail.com>
2022-01-05 00:27:16 +0530

    6th commit

--- public/app.js
+++ public/app.js
@@ -1,3 +1,12 @@
+// toggle button
+const toggleBtn = document.querySelector('.toggle-btn');
+const linkContainer = document.querySelector('.links-container');
+
+toggleBtn.addEventListener('click', () => {
+    toggleBtn.classList.toggle('active');
+    linkContainer.classList.toggle('show');
+})
+
 // links
 
 const links = document.querySelectorAll('.links');
